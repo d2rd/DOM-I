c4707b3635e02b8780c2aa652ee23a326a599bee
Glenn-David Daniel <d2rd@outlook.com>
2018-08-28 17:03:34 -0700

    Setup and selectors.js file

---
+++ js/selectors.js
@@ -0,0 +1,53 @@
+/*
+document.getElementById('idName')
+
+document.getElementsByClassName('className')
+
+document.getElementsByName('elementName')
+
+document.querySelector('queryString')
+
+document.querySelectorAll('queryString')
+
+document.createElement('elementName')
+*/
+
+
+// ANSWERS
+
+console.log(document.getElementById('logo-img'))
+
+/*
+===== DEMO CODEPEN =====
+// const header = document.querySelector('h1');
+// header.innerHTML = 'SOMETHING NEW UP HERE! STILL TREES'
+
+// const img = document.querySelector('.main-image');
+
+// img.src = 'https://media.treehugger.com/assets/images/2016/07/green-forest-trees.jpg.860x0_q70_crop-scale.jpg';
+
+// const paragraph = document.getElementById('para');
+// paragraph.classList.add('make-things-red');
+
+// paragraph.style.color = 'green';
+// paragraph.style.fontSize = '48px';
+
+const h2 = document.createElement('h2');
+h2.innerHTML = 'NEW CONTENT ABOUT TREES!'
+h2.classList.add('make-things-red')
+h2.style.fontSize = '26px'
+
+const img = document.createElement('img');
+img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Aspen-PopulusTremuloides-2001-09-27.jpg/220px-Aspen-PopulusTremuloides-2001-09-27.jpg"
+
+const contentDiv = document.querySelector('#content');
+contentDiv.appendChild(h2)
+contentDiv.appendChild(img)
+
+// console.log(contentDiv.children)
+
+// console.log(contentDiv.parentNode)
+
+// Array.from(contentDiv.children)
+
+*/
